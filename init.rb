89802126d2550fcec1cee3e4a7aeeef27b89f82d
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-02-15 20:09:27 +0000

    plugin template

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :x_backlogs_styleadapter do
+  name 'X Backlogs Styleadapter plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
