bad8747d55d946401500444a6b6a73798ad0d65f
Patrick Atamaniuk <patrick.atamaniuk@wibas.de>
2013-02-15 21:16:03 +0000

    fix plugin url

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :x_backlogs_styleadapter do
   author 'Patrick Atamaniuk'
   description 'Style patches for for Redmine Backlogs and Redmine'
   version '0.0.1'
-  url 'https://github.com/path/patrickatamaniuk/x_backlogs_styleadapter'
+  url 'https://github.com/patrickatamaniuk/x_backlogs_styleadapter'
   author_url 'https://github.com/patrickatamaniuk'
 
   require_dependency 'x_backlogs_styleadapter_header_hook.rb'
